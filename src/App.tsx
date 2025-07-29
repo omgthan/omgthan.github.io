31d51ab2c06985ffab5f3bad2774bc769baa94c8
Ethan Dirkes <eldirkes@gmail.com>
2025-07-29 14:21:31 -0500

    Temp landing

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+		<h1>Welcome!</h1>
+		<p>This page is currently a work in progress, so sorry! Come back at a later time while I
+			continue to work on my site in private until I decide it's in a state where it's ready to publish.
+		</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
